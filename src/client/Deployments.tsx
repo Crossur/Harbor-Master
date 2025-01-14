f4e47ca6dc17f78d29518c75ca04283fc3cb8658
Crossur <cruz24955@gmail.com>
2025-01-14 13:19:58 -0500

    cloudbuild test

--- src/client/Deployments.tsx
+++ src/client/Deployments.tsx
@@ -301,7 +301,7 @@ const Deployments: React.FC = () => {
           type='submit'
           className='mt-4 bg-custom-blue text-white py-3 px-6 rounded-lg shadow-lg font-semibold'
         >
-          {!loading ? 'DeployTest':'Deploying...'}
+          {!loading ? 'DeployTest2':'Deploying...'}
         </button>
         {loading && 
         <ProjectModal
